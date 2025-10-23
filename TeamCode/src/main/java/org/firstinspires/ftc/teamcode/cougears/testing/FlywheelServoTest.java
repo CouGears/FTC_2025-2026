dad9e12c1f10a34f37cdc1ab4ea219bb6ce84c7c
rpowergso <rpowergso@gmail.com>
2025-10-23 17:39:27 -0400

    chanegd FlywheelServoTest to make it work for 1 wheel shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cougears/testing/FlywheelServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cougears/testing/FlywheelServoTest.java
@@ -2,102 +2,72 @@ package org.firstinspires.ftc.teamcode.cougears.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp(name = "FlywheelServoTest", group = "testing")
 public class FlywheelServoTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // --- TUNABLE VARIABLES ---
-        int speedMin = 10;                // minimum flywheel speed percent
-        int speedMax = 100;               // maximum flywheel speed percent
-        double flywheelStep = 2;          // how much speed changes per loop (right stick)
-        double servoStep = 0.02;          // how much servo max changes per loop (left stick)
+        DcMotorEx motor1 = hardwareMap.get(DcMotorEx.class, "motor1");
 
-        // --- Hardware mapping ---
-        DcMotor motor1 = hardwareMap.get(DcMotor.class, "motor1");
-        DcMotor motor2 = hardwareMap.get(DcMotor.class, "motor2");
-        Servo servo1 = hardwareMap.get(Servo.class, "servo1");
-        Servo servo2 = hardwareMap.get(Servo.class, "servo2");
+        motor1.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        motor1.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
-        // --- Initial states ---
-        double flywheelPower = 0.0;
-        boolean flywheelsOn = false;
+        boolean motorOn = false;
         boolean directionReversed = false;
-        int speedPercent = 50; // start at 50%
-        double servo2Min = 0.0;
-        double servo2Max = 0.2; // initial max open position
-        double servo2Pos = servo2Min;
+        double powerPercent = 50; // start at 50%
+        double powerStep = 3;     // each press changes by 3%
 
-        // Servo1 positions (gate)
-        double servo1OpenPos = 0.5;
-        double servo1ClosedPos = 1.0;
-
-        servo1.setPosition(servo1OpenPos);
-        servo2.setPosition(servo2Pos);
-
-        telemetry.addLine("Ready to start: FlywheelServoTest");
+        telemetry.addLine("Ready to start: Motor1PowerControl");
         telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
-            // --- Flywheel toggle ---
+            // Toggle motor on/off
             if (gamepad1.a) {
-                flywheelsOn = !flywheelsOn;
-                sleep(250); // debounce
+                motorOn = !motorOn;
+                sleep(250);
             }
+
+            // Toggle direction
             if (gamepad1.b) {
                 directionReversed = !directionReversed;
                 sleep(250);
             }
 
-            // --- Right stick controls flywheel speed ---
-            double stickYRight = -gamepad1.right_stick_y; // up is positive
-            if (Math.abs(stickYRight) > 0.1) {           // deadzone
-                speedPercent += stickYRight * flywheelStep;
-                speedPercent = (int) Math.max(speedMin, Math.min(speedPercent, speedMax));
+            // Increase power
+            if (gamepad1.x) {
+                powerPercent += powerStep;
+                sleep(200);
             }
 
-            // Compute flywheel power
-            flywheelPower = flywheelsOn ? (speedPercent / 100.0) : 0.0;
-            double power1 = directionReversed ? -flywheelPower : flywheelPower;
-            double power2 = -power1;
-            motor1.setPower(power1);
-            motor2.setPower(power2);
-
-            // --- Servo1 (gate) ---
-            if (gamepad1.right_bumper) {
-                servo1.setPosition(servo1ClosedPos);
-            } else if (gamepad1.right_trigger > 0.5) {
-                servo1.setPosition(servo1OpenPos);
+            // Decrease power
+            if (gamepad1.y) {
+                powerPercent -= powerStep;
+                sleep(200);
             }
 
-            // --- Left stick Y modifies servo2 max position ---
-            double stickYLeft = -gamepad1.left_stick_y; // up is positive
-            if (Math.abs(stickYLeft) > 0.1) {          // deadzone
-                servo2Max += stickYLeft * servoStep;
-                servo2Max = Math.max(0.0, Math.min(servo2Max, 1.0)); // clamp 0..1
+            // Clamp power between 0% and 100%
+            powerPercent = Math.max(0, Math.min(powerPercent, 100));
+
+            // Compute final power
+            double power = (powerPercent / 100.0);
+            if (directionReversed) power *= -1;
+
+            // Apply or stop
+            if (motorOn) {
+                motor1.setPower(power);
+            } else {
+                motor1.setPower(0);
             }
 
-            // --- Left bumper/trigger sets servo2 open/close ---
-            if (gamepad1.left_bumper) {
-                servo2Pos = servo2Min; // closed
-            } else if (gamepad1.left_trigger > 0.5) {
-                servo2Pos = servo2Max; // open
-            }
-            servo2.setPosition(servo2Pos);
-
-            // --- Telemetry ---
-            telemetry.addLine("Flywheel + Servo Test");
-            telemetry.addData("Flywheels On", flywheelsOn);
+            telemetry.addLine("Motor1 Power Test");
+            telemetry.addData("Motor On", motorOn);
             telemetry.addData("Direction Reversed", directionReversed);
-            telemetry.addData("Speed (%)", speedPercent);
-            telemetry.addData("Servo1 Pos", servo1.getPosition());
-            telemetry.addData("Servo2 Pos", servo2Pos);
-            telemetry.addData("Servo2 Max", servo2Max);
+            telemetry.addData("Power (%)", powerPercent);
+            telemetry.addData("Applied Power", motorOn ? power : 0);
             telemetry.update();
         }
     }
