693cbdf60f56880f2bdd8e4b25335b2905848322
rpowergso <rpowergso@gmail.com>
2025-12-11 12:17:00 -0500

    Added hold down functionality to component test, adding a pidf auto tuner dont know if it works?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cougears/testing/PIDFAutoTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cougears/testing/PIDFAutoTuner.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.cougears.testing;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+@Disabled
 @TeleOp(name = "PIDF Auto Tuner", group = "Testing")
 public class PIDFAutoTuner extends LinearOpMode {
 
